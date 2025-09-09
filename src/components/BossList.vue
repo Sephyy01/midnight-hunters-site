ccc22356f30d9edacfe27f3eec9bf7b4492b8e80
Juan <juan@example.com>
2025-09-08 21:01:27 -0300

    🔧 Update CSV URLs in BossList component for improved data access and reliability

--- src/components/BossList.vue
+++ src/components/BossList.vue
@@ -107,7 +107,7 @@ export default {
             loading: false,
             error: null,
             lastUpdate: null,
-            csvUrl: 'https://corsproxy.io/?https://docs.google.com/spreadsheets/d/e/2PACX-1vSkPumc5StGjCOnwkq_vnorgme3AE4RX2myf3mQWi4Z7si-MlRahFECfxBRyItYt2Ft1ibY9HdqzdWw/pub?output=csv',
+            csvUrl: 'https://docs.google.com/spreadsheets/d/1pQ1OoZkp8gj_nkyW0INQ4HCIw0cWQ80FOFxLhF3r55c/export?format=csv&gid=314246739',
             spreadsheetUrl: 'https://docs.google.com/spreadsheets/d/1pQ1OoZkp8gj_nkyW0INQ4HCIw0cWQ80FOFxLhF3r55c/edit?gid=314246739#gid=314246739'
         }
     },
@@ -134,9 +134,9 @@ export default {
 
                 // Lista de URLs para tentar (fallbacks)
                 const urls = [
-                    `https://cors-anywhere.herokuapp.com/https://docs.google.com/spreadsheets/d/e/2PACX-1vSkPumc5StGjCOnwkq_vnorgme3AE4RX2myf3mQWi4Z7si-MlRahFECfxBRyItYt2Ft1ibY9HdqzdWw/pub?output=csv`,
-                    `https://api.allorigins.win/get?url=${encodeURIComponent('https://docs.google.com/spreadsheets/d/e/2PACX-1vSkPumc5StGjCOnwkq_vnorgme3AE4RX2myf3mQWi4Z7si-MlRahFECfxBRyItYt2Ft1ibY9HdqzdWw/pub?output=csv')}`,
-                    `https://corsproxy.io/?https://docs.google.com/spreadsheets/d/e/2PACX-1vSkPumc5StGjCOnwkq_vnorgme3AE4RX2myf3mQWi4Z7si-MlRahFECfxBRyItYt2Ft1ibY9HdqzdWw/pub?output=csv`
+                    `https://cors-anywhere.herokuapp.com/https://docs.google.com/spreadsheets/d/1pQ1OoZkp8gj_nkyW0INQ4HCIw0cWQ80FOFxLhF3r55c/export?format=csv&gid=314246739`,
+                    `https://api.allorigins.win/get?url=${encodeURIComponent('https://docs.google.com/spreadsheets/d/1pQ1OoZkp8gj_nkyW0INQ4HCIw0cWQ80FOFxLhF3r55c/export?format=csv&gid=314246739')}`,
+                    `https://corsproxy.io/?https://docs.google.com/spreadsheets/d/1pQ1OoZkp8gj_nkyW0INQ4HCIw0cWQ80FOFxLhF3r55c/export?format=csv&gid=314246739`
                 ]
 
                 for (let i = 0; i < urls.length; i++) {
